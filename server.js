a236776215cbe814556cab6ff1243df1833e917d
Dean Parrish <parrish.de@gmail.com>
2022-01-20 21:01:40 -0500

    Standardizing better error messages

--- server.js
+++ server.js
@@ -1,50 +1,45 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const http = require('http');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const http = require("http");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
+require("dotenv").config();
 
-const whitelist = ['http://localhost:8100',];
+const whitelist = ["http://localhost:8100"];
 
-    const corsOptionsDelegate = (req, callback) => {
-     let corsOptions;
+const corsOptionsDelegate = (req, callback) => {
+  let corsOptions;
 
-    // let isDomainAllowed = whitelist.indexOf(req.header('Origin')) !== -1;
+  // let isDomainAllowed = whitelist.indexOf(req.header('Origin')) !== -1;
 
-    // if (isDomainAllowed) {
-    //     // Enable CORS for this request
-    //     corsOptions = { origin: true }
-    // } else {
-    //     // Disable CORS for this request
-    //     corsOptions = { origin: false }
-    // }
-    callback(null, corsOptions)
-}
+  // if (isDomainAllowed) {
+  //     // Enable CORS for this request
+  //     corsOptions = { origin: true }
+  // } else {
+  //     // Disable CORS for this request
+  //     corsOptions = { origin: false }
+  // }
+  callback(null, corsOptions);
+};
 
 // API file for interacting with MongoDB
-const api = require('./server/routes/api');
+const api = require("./server/routes/api");
 
 // Parsers
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Angular DIST output folder
-app.use(express.static(path.join(__dirname, 'dist/my-test-angular-app')));
-app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static(path.join(__dirname, "dist/my-test-angular-app")));
+app.use(express.static(path.join(__dirname, "src")));
 
 // API location
-//app.use('/api', cors(corsOptionsDelegate),  api);
-app.use('/api',  api);
-
-// Send all other requests to the Angular app
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/my-test-angular-app/index.html'));
-});
+app.use("/api", api);
 
 //Set Port
-const port = process.env.PORT || '3000';
-app.set('port', port);
+const port = process.env.PORT || "3000";
+app.set("port", port);
 
 const server = http.createServer(app);
 
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,279 +1,361 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require("mongodb").MongoClient;
 
-const req = require('request');
+const req = require("request");
 
 // Connect
 const connection = (closure) => {
-    return MongoClient.connect('mongodb://mongo:27017/test', { useNewUrlParser: true }, (err, client) => {
-    if (err) return console.log(err);
-    closure(client.db());
-    });
+  return MongoClient.connect(
+    "mongodb://" + process.env.MONGO_ENV + ":27017/test",
+    { useNewUrlParser: true },
+    (err, client) => {
+      if (err) return console.log(err);
+      closure(client.db());
+    }
+  );
 };
 
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./../../portfoliowebsite-28f8a-firebase-adminsdk-8e4dv-6513fab54f.json");
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_CREDS)),
 });
 
-// const checkJwt = jwt({
-//     // Dynamically provide a signing key
-//     // based on the kid in the header and 
-//     // the signing keys provided by the JWKS endpoint.
-//     secret: jwksRsa.expressJwtSecret({
-//       cache: true,
-//       rateLimit: true,
-//       jwksRequestsPerMinute: 5,
-//       jwksUri: `https://dev-4zf9twtm.us.auth0.com/.well-known/jwks.json`
-//     }),
-  
-//     // Validate the audience and the issuer.
-//     //audience: 'https://deanparrish.net/',
-//     //issuer: `https://deanparrish.net/`,
-//     audience: 'portfoliowebsite-28f8a',
-//     issuer: 'https://securetoken.google.com/portfoliowebsite-28f8a',
-//     algorithms: ['RS256']
-//   });
-const checkJwt = function(idToken){
-    admin
-  .auth()
-  .verifyIdToken(idToken)
-  .then((decodedToken) => {
-    const uid = decodedToken.uid;
-    // ...
-  })
-  .catch((error) => {
-    // Handle error
-  });
-}
 
 // Error handling
-const sendError = (err, res) => {
-    response.status = 501;
-    response.message = typeof err == 'object' ? err.message : err;
-    res.status(501).json(response);
+const sendError = (err, res, code) => {
+  var errRes = {
+    status: code,
+    message: typeof err == "object" ? err.message : err,
+  };
+  res.status(code).json(errRes);
 };
 
 // Response handling
 let response = {
-    status: 200,
-    data: [],
-    message: null
+  status: 200,
+  data: [],
+  message: null,
 };
 
 var mongoose = require("mongoose");
-mongoose.Promise = global.Promise;mongoose.connect("mongodb://mongo:27017/test", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://" + process.env.MONGO_ENV + ":27017/test", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 var userSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    phoneNumber1: String,
-    phoneNumber2: String,
-    phoneNumber3: String,
-    address: String,
-    city: String,
-    state: String,
-    zip: String,
-    comments: String
-   });
+  firstName: String,
+  lastName: String,
+  phoneNumber1: String,
+  phoneNumber2: String,
+  phoneNumber3: String,
+  address: String,
+  city: String,
+  state: String,
+  zip: String,
+  comments: String,
+});
 
 var User = mongoose.model("customerInfo", userSchema);
 
 // Get users
-router.get('/users', (req, res) => {
-    admin.auth().verifyIdToken(req.header("Authorization")).then(result => {
+router.get("/users", (req, res) => {
+  admin
+    .auth()
+    .verifyIdToken(req.header("Authorization"))
+    .then(
+      (result) => {
         connection((db) => {
-            User.find({}).sort("firstName")
-                .then(users => {
-                    response.data = users;
-                    res.json(response);
-                })
-                .catch(err => sendError(err, res));
+          User.find({})
+            .sort("firstName")
+            .then((users) => {
+              response.data = users;
+              res.json(response);
+            })
+            .catch((err) => sendError(err, res, 500));
         });
-    }, err => {
-        if(err.code === "auth/argument-error" || err.code === "auth/id-token-expired"){
-            res.status(403);
-            res.json(response);
-        }else{
-            res.status(500);
-            response.data = err;
-            res.json(response);
+      },
+      (err) => {
+        if (
+          err.code === "auth/argument-error" ||
+          err.code === "auth/id-token-expired"
+        ) {
+          sendError(err, res, 403);
+        } else {
+          sendError(err, res, 500);
         }
-        
-        
-    })   
+      }
+    );
 });
 
-router.post('/insertuser', (req, res) => {
-    admin.auth().verifyIdToken(req.header("Authorization")).then(result => {
+router.post("/insertuser", (req, res) => {
+  admin
+    .auth()
+    .verifyIdToken(req.header("Authorization"))
+    .then(
+      (result) => {
         connection((db) => {
-            var data = new User(req.body);
-            data.save()
-            .then(item => {
-                response.data = item;
-                res.json(response)
-                })
-                .catch(err => {
-                    console.log("not saved")
-                    sendError(err, res);
-                });
+          var data = new User(req.body);
+          data
+            .save()
+            .then((item) => {
+              response.data = item;
+              res.json(response);
+            })
+            .catch((err) => {
+              sendError(err, res, 500);
+            });
         });
-    }) 
-    
+      },
+      (err) => {
+        if (
+          err.code === "auth/argument-error" ||
+          err.code === "auth/id-token-expired"
+        ) {
+          sendError(err, res, 403);
+        } else {
+          sendError(err, res, 500);
+        }
+      }
+    );
 });
 
 router.put("/updatecustomer/:id", (req, res) => {
-    admin.auth().verifyIdToken(req.header("Authorization")).then(result => {
+  admin
+    .auth()
+    .verifyIdToken(req.header("Authorization"))
+    .then(
+      (result) => {
         connection((db) => {
-            User.updateOne({_id: req.params.id}, req.body,{upsert: true})
-                .then(item => {
-                    response.data = item;
-                    res.json(response);
-                })
-                .catch(err => {
-                    console.log("not saved")
-                    sendError(err, res);
-                });
-            res.data = "in update";
+          User.updateOne({ _id: req.params.id }, req.body, { upsert: true })
+            .then((item) => {
+              response.data = item;
+              res.json(response);
+            })
+            .catch((err) => {
+              sendError(err, res, 500);
+            });
+          res.data = "in update";
         });
-    })     
+      },
+      (err) => {
+        if (
+          err.code === "auth/argument-error" ||
+          err.code === "auth/id-token-expired"
+        ) {
+          sendError(err, res, 403);
+        } else {
+          sendError(err, res, 500);
+        }
+      }
+    );
 });
 
 router.post("/deletecustomer/:id", (req, res) => {
-    admin.auth().verifyIdToken(req.header("Authorization")).then(result => {
+  admin
+    .auth()
+    .verifyIdToken(req.header("Authorization"))
+    .then(
+      (result) => {
         connection((db) => {
-            User.deleteOne({_id: req.params.id})
-                .then(item => {
-                    response.data = item;
-                    res.json(response);
-                })
-                .catch(err => {
-                    console.log("not deleted")
-                    sendError(err, res);
-                });
-            res.data = "in delete";
+          User.deleteOne({ _id: req.params.id })
+            .then((item) => {
+              response.data = item;
+              res.json(response);
+            })
+            .catch((err) => {
+              sendError(err, res, 500);
+            });
         });
-    }) 
-    
-         
-    
+      },
+      (err) => {
+        if (
+          err.code === "auth/argument-error" ||
+          err.code === "auth/id-token-expired"
+        ) {
+          sendError(err, res, 403);
+        } else {
+          sendError(err, res, 500);
+        }
+      }
+    );
 });
 //BEGIN RECIPE
 
-
 var recipeSchema = new mongoose.Schema({
-    name: String,
-    category: String,
-    ingredients: Array,
-    steps: Array,
-    link: String,
-    userID: String,
-    isPrivate: Boolean,
-    description: String
+  name: String,
+  category: String,
+  ingredients: Array,
+  steps: Array,
+  link: String,
+  userID: String,
+  isPrivate: Boolean,
+  description: String,
 });
 
 var Recipe = mongoose.model("recipes", recipeSchema);
 
 // Get recipes
-router.get('/recipes', (req, res) => {
-    connection((db) => {
-        db.collection('recipes')
-            .find()
+router.get("/recipes", (req, res) => {
+  connection((db) => {
+    db.collection("recipes")
+      .find()
+      .toArray()
+      .then((recipes) => {
+        if (req.header("Authorization") != null) {
+          admin
+            .auth()
+            .verifyIdToken(req.header("Authorization"))
+            .then(
+              (decodedToken) => {
+                var pubArray = recipes.filter(function (recipe) {
+                  return recipe.isPrivate == false;
+                });
+                var privArray = recipes.filter(function (recipe) {
+                  return (
+                    recipe.isPrivate == true &&
+                    recipe.userID == decodedToken.uid
+                  );
+                });
+
+                response.data = [...pubArray, ...privArray];
+                res.json(response);
+              },
+              (err) => {
+                if (
+                  err.code === "auth/argument-error" ||
+                  err.code === "auth/id-token-expired"
+                ) {
+                  sendError(err, res, 403);
+                } else {
+                  sendError(err, res, 500);
+                }
+              }
+            );
+        } else {
+          //non authenicated route
+          var pubArray = recipes.filter(function (recipe) {
+            return recipe.isPrivate == false;
+          });
+          response.data = pubArray;
+          res.json(response);
+        }
+      })
+      .catch((err) => {
+        sendError(err, res, 500);
+      });
+  });
+});
+
+router.get("/userrecipes/:id", (req, res) => {
+  admin
+    .auth()
+    .verifyIdToken(req.header("Authorization"))
+    .then(
+      (result) => {
+        connection((db) => {
+          db.collection("recipes")
+            .find({ userID: req.params.id })
             .toArray()
             .then((recipes) => {
-                
-                if (req.header("Authorization") != null) {
-                    admin.auth().verifyIdToken(req.header("Authorization")).then(decodedToken => {
-                        var pubArray = recipes.filter(function(recipe){
-                            return recipe.isPrivate == false;
-                        });
-                        var privArray = recipes.filter(function(recipe){
-                            return recipe.isPrivate == true && recipe.userID == decodedToken.uid;
-                        });
-
-                        response.data = [...pubArray, ...privArray];
-                        res.json(response);
-                    });
-                }else{//non authenicated route
-                    var pubArray = recipes.filter(function(recipe){
-                        return recipe.isPrivate == false;
-                    });
-                    response.data = pubArray;
-                    res.json(response);
-                }
-                
+              response.data = recipes;
+              res.json(response);
             })
             .catch((err) => {
-                sendError(err, res);
+              sendError(err, res, 500);
             });
-    });
-});
-
-router.get('/userrecipes/:id', (req, res) => {
-    admin.auth().verifyIdToken(req.header("Authorization")).then(result => {
-        connection((db) => {
-            db.collection("recipes").find({"userID": req.params.id}).toArray().then((recipes) =>{
-                response.data = recipes;
-                res.json(response);
-            })
-            .catch((err) => {
-                sendError(err, res);
-            })
         });
-    });
-})
-
-router.post('/insertrecipe',  (req, res) => {
-    admin.auth().verifyIdToken(req.header("Authorization")).then(result => {
-    connection((db) => {
-        var data = new Recipe(req.body);
-        data.save()
-        .then(item => {
-            response.data = item;
-            res.json(response)
-            })
-            .catch(err => {
-                sendError(err, res);
-            });
-    });
-}, err => {
-    sendError(err, res);
-});
+      },
+      (err) => {
+        if (
+          err.code === "auth/argument-error" ||
+          err.code === "auth/id-token-expired"
+        ) {
+          sendError(err, res, 403);
+        } else {
+          sendError(err, res, 500);
+        }
+      }
+    );
 });
 
-router.put("/updaterecipe/:id",  (req, res) => {
-    admin.auth().verifyIdToken(req.header("Authorization")).then(result => {
-    connection((db) => {
-        Recipe.updateOne({_id: req.params.id}, req.body,{upsert: true})
-            .then(item => {
-                response.data = item;
-                res.json(response);
+router.post("/insertrecipe", (req, res) => {
+  admin
+    .auth()
+    .verifyIdToken(req.header("Authorization"))
+    .then(
+      (result) => {
+        connection((db) => {
+          var data = new Recipe(req.body);
+          data
+            .save()
+            .then((item) => {
+              response.data = item;
+              res.json(response);
             })
-            .catch(err => {
-                sendError(err, res);
+            .catch((err) => {
+              sendError(err, res, 500);
             });
-        res.data = "in update";
-    });
-}); 
-    
+        });
+      },
+      (err) => {
+        if (
+          err.code === "auth/argument-error" ||
+          err.code === "auth/id-token-expired"
+        ) {
+          sendError(err, res, 403);
+        } else {
+          sendError(err, res, 500);
+        }
+      }
+    );
+});
+
+router.put("/updaterecipe/:id", (req, res) => {
+  admin
+    .auth()
+    .verifyIdToken(req.header("Authorization"))
+    .then(
+      (result) => {
+        connection((db) => {
+          Recipe.updateOne({ _id: req.params.id }, req.body, { upsert: true })
+            .then((item) => {
+              response.data = item;
+              res.json(response);
+            })
+            .catch((err) => {
+              sendError(err, res, 500);
+            });
+          res.data = "in update";
+        });
+      },
+      (err) => {
+        if (
+          err.code === "auth/argument-error" ||
+          err.code === "auth/id-token-expired"
+        ) {
+          sendError(err, res, 403);
+        } else {
+          sendError(err, res, 500);
+        }
+      }
+    );
 });
 
 //begin users
 
 var userSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    email: String,
-    steps: Array,
-    link: String,
-    user: String
+  firstName: String,
+  lastName: String,
+  email: String,
+  steps: Array,
+  link: String,
+  user: String,
 });
 
 var Recipe = mongoose.model("recipes", recipeSchema);
-
 
 module.exports = router;
