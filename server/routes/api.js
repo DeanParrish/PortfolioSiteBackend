aa4d70cf1bfd0f20830a53870d4f7ae29c961fa9
Dean Parrish <parrish.de@gmail.com>
2022-01-31 22:19:58 -0500

    Testing

--- server/routes/api.js
+++ server/routes/api.js
@@ -17,8 +17,7 @@ const connection = (closure) => {
 };
 
 var admin = require("firebase-admin");
-console.log(process.env.FIREBASE_CREDS);
-console.log(process.env.MONGO_ENV);
+console.log("MONGO_ENV: " + process.env.MONGO_ENV);
 
 admin.initializeApp({
   credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_CREDS)),
